aced39b32ef026c94371151f13398cf3e55ae0e6
ShifatHasanGNS <shifathasangns@gmail.com>
2020-06-16 19:13:18 +0600

    Modified

--- Python/palindromic_number.py
+++ Python/palindromic_number.py
@@ -1,17 +1,23 @@
 from math import log10, floor, ceil
 
 def find_palindromic_number(n):
-    a = 2*floor(log10(n))
-    # len = number of digits in the n-th palindromic number
-    len = a+(2*(1- floor((2*(10**(a/2))-2)/n)))-(1-floor(n/(11*(10**(((a+2*(1-floor((2*(10**(a/2))-2)/n)))-2)/2))-1)))
-    r = int(n - ((2+(9*((len-1)%2)))*(10**(((len-1)-((len-1)%2))/2))-2))
-    N1 = int((10**((ceil(len/2))-1)) + r - 1)
-    rN1 = int(str(floor(N1/(10**(len%2))))[::-1])
-    # N = n-th palindromic number
-    N = N1*(10**(floor(len/2))) + rN1
+
+    r = floor(log10(n))
+    s = floor((2*(10**r) - 2)/n)
+    t = r - s
+    d = 1 + (2*t) + floor(n/((11*(10**t)) - 1))
+    a = d % 2
+    u = floor((d - 1)/2)
+    m = (n + 2) - ((11 - 9*a)*(10**u))
+    N_o = (10**u) + m - 1
+    N_r = int(str(floor(N_o/(10**a)))[::-1])
+    N = N_o*(10**floor(d/2)) + N_r
+    
     return N
 
+
 # TEST
+
 while True:
     print(find_palindromic_number(int(input('>>> '))))
     print()
