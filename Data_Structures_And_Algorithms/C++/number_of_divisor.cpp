2ac14bb5a5fa445f9ffef648159dd967a6c65d27
ShifatHasanGNS <shifathasangns@gmail.com>
2020-07-01 19:15:19 +0600

    Modified

---
+++ Data_Structures_And_Algorithms/C++/number_of_divisor.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool is_leapyear(int year)
+{
+    return (year % 400 == 0 || (year % 100 !=0 && year % 4 == 0)) ? true : false;
+}
+
+int main(int argc, char const *argv[])
+{
+    int year;
+    while(true)
+    {
+        cout << ">>> ";
+        cin >> year;
+        if(is_leapyear(year)) cout << year << "  --->  Leapyear" << endl;
+        else cout << year << "  --->  Not Leapyear" << endl;
+    }
+    return 0;
+}
