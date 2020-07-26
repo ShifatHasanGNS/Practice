335d69375b9abc3a562254965d02c16fc6a74a31
ShifatHasanGNS <shifathasangns@gmail.com>
2020-07-26 21:13:49 +0600

    Added Some Stuffs

---
+++ JavaScript/Truthy_and_Falsy.js
@@ -0,0 +1,20 @@
+// Falsy Values:
+// undefined
+// null
+// 0
+// NaN
+
+var user = "2";
+
+if (2 == user) {
+    console.log("Condition is True... (==)");
+}
+
+if (2 === user) {
+    console.log("Condition is True... (===)");
+}
+
+console.log(2 + 2);
+console.log("2" + 2);
+console.log(2 + "2");
+console.log("2" + "2");
