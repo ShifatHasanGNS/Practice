422b9694f9deb152be1822369ee7003442d65ddd
ShifatHasanGNS <shifathasangns@gmail.com>
2020-11-27 15:25:31 +0600

    Added Something

---
+++ C++/Others/phi_of_n.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int phi(int n)
+{
+    int ret = n;
+    for (int i = 2; i*i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            // 'i' is a dividing 'n'
+            while (n % i == 0) n /= i; // dividing all the factors of 'i'
+            // same as: ret * (1 - 1/p)
+            ret -= ret/i;
+        }
+    }
+    if (n > 1) ret -= ret/n; // there can be only one prime greater... then sqrt(n) that divides 'n'
+    return ret;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "phi(" << n << ") = " << phi(n) << endl;
+    return 0;
+}
