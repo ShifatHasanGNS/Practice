fa55a4f516729bb3d7b925b8b75d84df08024078
ShifatHasanGNS <shifathasangns@gmail.com>
2022-02-04 19:57:54 +0600

    Modified

--- Rust/variable.rs
+++ Rust/variable.rs
@@ -2,6 +2,8 @@ fn main()
 {
     let a: i8 = 5;
     let b: &str = "hello";
+    let c: String = String::from("My name is Md. Shifat Hasan");
 
     println!("{1} {0}", a, b);
+    println!("{:?}", c);
 }
