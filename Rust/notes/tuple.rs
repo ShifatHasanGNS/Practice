50c906e39fca1d0de988981d396b87769026ecf3
ShifatHasanGNS <shifathasangns@gmail.com>
2022-08-20 22:48:19 +0600

    Added Something

---
+++ Rust/notes/tuple.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let p = (5, 6);
+    let a = ("text", 10);
+
+    println!("\n p = {:#?} \n and \n a = {:#?}", p, a);
+
+    let array = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]];
+
+    println!("array = {:?}", array);
+}
